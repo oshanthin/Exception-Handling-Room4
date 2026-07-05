03b6bb3023ee02fb65ff8b86f77d84ba17ccee13
oliylk <oshanthi.liyanage@ifs.com>
2026-07-05 18:31:56 +0530

    C file

--- Exercise C.py
+++ Exercise C.py
@@ -1,2 +1,26 @@
-text = "Hello World"
-print(text.append("!"))
+#AttributeError
+#text = "Hello World"
+#print(text.append("!"))
+
+'''
+try:
+    text = "Hello World"
+    text.append("!")
+except AttributeError:
+    print("You tried to use a method that doesn't exist for this object.")
+'''
+#ImportError / ModuleNotFoundError
+'''
+import maths
+print(math.sqrt(16))
+'''
+
+
+'''
+try:
+    import math
+except ModuleNotFoundError:
+    print("Module not found. Check spelling or install it.")
+print(math.sqrt(16))
+'''
+#RECURSIONERROR
